35facfed0be3eebb1f6aba4319a05307e68bd4f2
Jazz <mharris42@elon.edu>
2022-11-16 00:10:24 -0500

    updated loginpage url

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -27,7 +27,7 @@ const StyledLoginButton = styled.a`
 const LOGIN_URI =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:4444/login'
-    : 'https://spotifyapp-jazzharris.herokuapp.com/login';
+    : 'https://spotifyappjh.onrender.com/login';
 
 const Login = () => (
   <StyledLoginContainer>
